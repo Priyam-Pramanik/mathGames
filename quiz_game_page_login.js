cdb25d616170c9d0a488d112dec6f73ffbcaa522
Priyam-Pramanik <86661996+Priyam-Pramanik@users.noreply.github.com>
2022-05-23 17:44:42 +0200

    Add files via upload

---
+++ quiz_game_page_login.js
@@ -0,0 +1,10 @@
+function Useradd(){
+    player_1_name = document.getElementById("player_1_name").value;
+    player_2_name = document.getElementById("player_2_name").value;
+    
+    localStorage.setItem("player_1_name", player_1_name);
+    localStorage.setItem("player_2_name", player_2_name);
+    
+    window.location.replace("quiz_game_page.html");
+    
+    }
